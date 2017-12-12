019ce29a2214eff8928f1cffe17c2d9f3623ebd9
Sarah Schaller <sarcsch@aol.com>
2017-12-12 00:50:12 +0000

    automatically backed up by learn

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,7 +2,7 @@ class Doctor < ActiveRecord::Base
   has_many :appointments
   has_many :patients, through: :appointments
 
-  def num_of_appointments
+  def num_of_appts
     self.appointments.count
   end
 end
